e856802a8b6299c0fda825e6700203e600137cd6
just1courtois <59601373+just1courtois@users.noreply.github.com>
2022-03-23 16:22:23 +0100

    PyQT5 fix
    Propose de lancer l'ui après une première execution

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import GUI
 import TD
+import sys
 import numpy as np
 
 
@@ -10,10 +11,30 @@ def main(text1 = None, text2 = None):
     common_words, length = TD.excutable(text1, text2)
     print(common_words)
     print(length)
+    
+def mainGUI(text1 = None, text2 = None):
+    if (text1 == None):
+        GUI.main()
+    common_words, length = TD.excutable(text1, text2)
+    print(common_words)
+    print(length)
 
+##without GUI
 if __name__ == "__main__":
     main()
 
+    
+##launch GUI
+if __name__ == "__main__":
+    while True:
+        yn = input("Try GUI now ? [Y/N] : ")
+        if (yn == "Y"):
+            break
+        elif (yn == "N"):
+            sys.exit()
+        else:
+            continue
+    mainGUI()
 
 
 
